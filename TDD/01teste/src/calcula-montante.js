d685e7766ad37b7003db437b4b2aa818aaa80aed
squintal73 <sidnei_quintal@yahoo.com.br>
2023-07-16 22:53:47 -0300

    install eslint

--- TDD/01teste/src/calcula-montante.js
+++ TDD/01teste/src/calcula-montante.js
@@ -1,8 +1,8 @@
-function calculaMontante(capital, taxa, periodo){
-    let montante = capital * Math.pow((1 + taxa), periodo -1);
-    return montante; 
+function calculaMontante (capital, taxa, periodo) {
+  const montante = capital * Math.pow((1 + taxa), periodo - 1)
+  return montante
 }
 
 module.exports = {
-    calculaMontante,
+  calculaMontante
 }
--- TDD/01teste/test/calculo_montante.test.js
+++ TDD/01teste/test/calculo_montante.test.js
@@ -1,8 +1,7 @@
-const calculaValor = require ('../src/calcula-montante')
-
+const calculaValor = require('../src/calcula-montante')
 test('com uma prestacao o montante e igual ao capital', () => {
-    // Operação 
-    const montante = calculaValor.calculaMontante(100, 0.175, 1);
-    // Resultado do comportamento esperado.
-    expect(montante).toBe(100);
+  // Operação
+  const montante = calculaValor.calculaMontante(100, 0.175, 1)
+  // Resultado do comportamento esperado.
+expect(montante).toBe(100)
 })
